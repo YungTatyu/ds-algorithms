c2610f97d57df42e09ca42ce5c6f951f90716b59
tachu <roatyungtatyu@gmail.com>
2025-02-21 14:25:45 +0900

    add: optimized function

--- recursion/power/power.c
+++ recursion/power/power.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ * time complexity O(n)
+ * space complexity O(n)
+ */
 int power(int base, int exponent) {
   if (exponent == 0) {
     return 1;
@@ -7,7 +11,25 @@ int power(int base, int exponent) {
   return power(base, exponent - 1) * base;
 }
 
+/**
+ * time complexity O(log n)
+ * space complexity O(log n)
+ * because function is calling itself with n/2 every time.
+ */
+int optimized_power(int base, int exponent) {
+  if (exponent == 0) {
+    return 1;
+  }
+  if (exponent % 2 == 0) {
+    return optimized_power(base * base, exponent / 2);
+  } else {
+    return base * optimized_power(base * base, (exponent - 1) / 2);
+  }
+}
 int main(int argc, char *argv[]) {
   printf("%d\n", power(2, 9));
+  printf("%d\n", optimized_power(2, 9));
+  printf("%d\n", power(9, 3));
+  printf("%d\n", optimized_power(9, 3));
   return 0;
 }
