34be377948faba6232dfa06b9f5e2def312f1f02
tachu <roatyungtatyu@gmail.com>
2025-02-22 14:51:10 +0900

    add: prims_mst

--- graphs/prims_mst/prims_mst.cpp
+++ graphs/prims_mst/prims_mst.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <limits.h>
 #define LENGTH 8
 #define I INT_MAX // INF as I
@@ -6,12 +7,12 @@ void PrimsMST(int cost[][LENGTH], int n) {
   int min = INT_MAX;
   int min_fi, min_si;
   int fi, si;
-  int t[2][LENGTH - 2] = {0}; // solution tree
-  int near[LENGTH] = {I};
+  int t[2][LENGTH - 2] = {}; // solution tree
+  int near[LENGTH];
 
   /*find the smallest wight*/
-  for (fi = 1; fi < LENGTH; ++fi) {
-    for (si = fi; si < LENGTH; ++si) {
+  for (fi = 1; fi <= n; ++fi) {
+    for (si = fi; si <= n; ++si) {
       if (cost[fi][si] < min) {
         min_fi = fi;
         min_si = si;
@@ -19,8 +20,50 @@ void PrimsMST(int cost[][LENGTH], int n) {
       }
     }
   }
-  t[0][0] = fi;
-  t[1][0] = si;
+  /*pick two vertex*/
+  t[0][0] = min_fi;
+  t[1][0] = min_si;
+  near[min_fi] = near[min_si] = 0; // 0 as already selected vertex
+
+  /*init near array*/
+  /*find the near vertex connecting to the two chosen vertex*/
+  /*index implies vertex*/
+  for (int i = 1; i <= n; ++i) {
+    if (near[i] == 0) {
+      continue;
+    }
+    if (cost[i][min_fi] < cost[i][min_si]) {
+      near[i] = min_fi;
+    } else {
+      near[i] = min_si;
+    }
+  }
+
+  for (int ti = 1; ti < n - 1; ++ti) {
+    min = INT_MAX;
+    int k; // min vertex
+    for (int mi = 1; mi <= n; ++mi) {
+      if (near[mi] != 0 && cost[mi][near[mi]] < min) {
+        k = mi;
+        min = cost[mi][near[mi]];
+      }
+    }
+    std::cout << "ti:" << ti << " k:" << k << "\n";
+    t[0][ti] = k;
+    t[1][ti] = near[k];
+    near[k] = 0;
+
+    /*update near vertex*/
+    for (int i = 1; i <= n; ++i) {
+      if (near[i] != 0 && cost[i][k] < cost[i][near[i]]) {
+        near[i] = k;
+      }
+    }
+  }
+
+  for (int i = 0; i < n - 1; ++i) {
+    std::cout << "(" << t[0][i] << "," << t[1][i] << ")\n";
+  }
 }
 
 int main() {
@@ -33,7 +76,7 @@ int main() {
       {I, 5, I, I, I, 20, I, I},   {I, I, 10, I, 14, 18, I, I},
   };
 
-  int n = sizeof(cost[0]) / sizeof(cost[0][0]) - 1;
+  int n = LENGTH - 1;
 
   PrimsMST(cost, n);
 
