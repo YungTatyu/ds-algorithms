686edda57a2d7e0df0032488f2d63304de36eef8
tachu <roatyungtatyu@gmail.com>
2025-02-23 18:23:54 +0900

    add: combination using loop

--- recursion/combination/combination.c
+++ recursion/combination/combination.c
@@ -1,5 +1,7 @@
 
-/*
+#include <stdio.h>
+
+/**
  * Pascal's Triangle:
  *
  *        1
@@ -14,7 +16,6 @@
  * 各行の要素は、直上の2つの数の和になっている。
  * 例: 6 = 3 + 3, 10 = 4 + 6
  */
-#include <stdio.h>
 int combinatin(int n, int r) {
   if (r == 0 || r == n) {
     return 1;
@@ -22,8 +23,34 @@ int combinatin(int n, int r) {
   return combinatin(n - 1, r - 1) + combinatin(n - 1, r);
 }
 
+int fact(int n) {
+  if (n == 0) {
+    return 1;
+  }
+  return fact(n - 1) * n;
+}
+
+/**
+ * Combination formula (nCr):
+ *
+ *          n!
+ * nCr = ------------
+ *        r! (n-r)!
+ *
+ * Example:
+ *   5C2 = 5! / (2! * (5-2)!)
+ *        = (5 × 4 × 3 × 2 × 1) / ((2 × 1) × (3 × 2 × 1))
+ *        = 10
+ */
+int combination_with_loop(int n, int r) {
+  int num = fact(n);
+  int den = fact(r) * fact(n - r);
+  return num / den;
+}
+
 int main(int argc, char *argv[]) {
   int num = 3;
+  printf("%d\n", combination_with_loop(3, 2));
   printf("%d\n", combinatin(3, 2));
   return 0;
 }
