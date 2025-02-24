1ecd8a0a1b9bac425fca516b9f86399b745d7032
tachu <roatyungtatyu@gmail.com>
2025-02-24 16:37:26 +0900

    init: array

---
+++ array/init/array.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+int main(int argc, char *argv[]) {
+  int a[5]; // contain garbage values
+  int b[4] = {0, 1, 2, 3};
+  int c[10] = {2, 4}; // the rest are initialized with zeros
+  int d[5] = {0};     // initialized with all zeros
+  int e[5] = {-1};    // initialized with all zeros
+  int arr[] = {0, 1, 2, 3};
+  return 0;
+}
