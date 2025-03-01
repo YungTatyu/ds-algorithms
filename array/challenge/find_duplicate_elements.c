4f7a2946fe691244a319f04dc1e5d555544c1b9b
tachu <roatyungtatyu@gmail.com>
2025-03-01 14:20:28 +0900

    add: dup count

--- array/challenge/find_duplicate_elements.c
+++ array/challenge/find_duplicate_elements.c
@@ -3,17 +3,25 @@
 #include <stdio.h>
 void find_duplicate_elements(const int *arr, size_t size) {
   int dup = arr[0] + 1;
+  size_t count = 0;
   for (size_t i = 0; i < size - 1; ++i) {
-    if (arr[i] == arr[i + 1] && arr[i] != dup) {
+    int is_dup = arr[i] == arr[i + 1];
+    if (is_dup && arr[i] != dup) {
       printf("%d is duplicate\n", arr[i]);
       dup = arr[i];
+      ++count;
+    }
+    if (is_dup) {
+      ++count;
     }
   }
+  printf("%zu duplicate\n", count);
 }
 
 int main(int argc, char *argv[]) {
 
-  int arr[] = {1, 2, 3, 4, 5, 6};
+  int arr[] = {1, 2, 3, 3, 4, 5, 6, 6, 6};
+
   find_duplicate_elements(arr, sizeof(arr) / sizeof(int));
   return 0;
 }
