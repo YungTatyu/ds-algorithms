57b7e75844179dbc48b9540f993aecd28b744aa3
tachu <roatyungtatyu@gmail.com>
2025-03-05 10:15:07 +0900

    fix: bug

--- string/find_dup.c
+++ string/find_dup.c
@@ -12,7 +12,8 @@ void find_dup_using_hashtable(const char *str) {
     if (!isalpha(str[i])) {
       continue;
     }
-    size_t index = islower(str[i]) ? str[i] - 'a' + 'z' + 1 : str[i] - 'A';
+    size_t index =
+        islower(str[i]) ? str[i] - 'a' + ('z' - 'a' + 1) : str[i] - 'A';
     ++h[index];
     if (h[index] > 1) {
       printf("duplicate value: %c %d times\n", str[i], h[index]);
@@ -27,7 +28,8 @@ void find_dup_using_bits(const char *str) {
     if (!isalpha(str[i])) {
       continue;
     }
-    long bit_shifts = islower(str[i]) ? str[i] - 'a' + 'z' + 1 : str[i] - 'A';
+    long bit_shifts =
+        islower(str[i]) ? str[i] - 'a' + ('z' - 'a' + 1) : str[i] - 'A';
     if (h & 1 << bit_shifts) {
       printf("duplicate value: %c\n", str[i]);
     }
