ede2c39217619089a35c76503289ea3482542d6c
tachu <roatyungtatyu@gmail.com>
2025-03-07 01:15:06 +0900

    add

--- string/permutations.c
+++ string/permutations.c
@@ -19,9 +19,28 @@ void permutations(const char *s, size_t level) {
   }
 }
 
+void swap(char *a, char *b) {
+  char tmp = *a;
+  *a = *b;
+  *b = tmp;
+}
+
+void permutations_modifying_s(char *s, size_t low, size_t high) {
+  if (low == high) {
+    printf("%s\n", s);
+    return;
+  }
+  for (size_t i = low; s[i] != '\0'; ++i) {
+    swap(&s[i], &s[low]);
+    permutations_modifying_s(s, low + 1, high);
+    swap(&s[i], &s[low]);
+  }
+}
+
 int main(int argc, char *argv[]) {
 
   permutations("abc", 0);
-  permutations("abcd", 0);
+  char s[] = "xyz";
+  permutations_modifying_s(s, 0, 2);
   return 0;
 }
