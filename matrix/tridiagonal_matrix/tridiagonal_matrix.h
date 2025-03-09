cdfb6d72f2c9de629c7bf399663905b205e23655
tachu <roatyungtatyu@gmail.com>
2025-03-10 01:17:05 +0900

    add: comments

--- matrix/tridiagonal_matrix/tridiagonal_matrix.h
+++ matrix/tridiagonal_matrix/tridiagonal_matrix.h
@@ -7,6 +7,27 @@ struct TridiagonalMatrix {
   size_t size;
 };
 
+/**
+ * Structure: TridiagonalMatrix
+ * Description: Represents a tridiagonal matrix, which is a square matrix
+ *              with non-zero elements only on the main diagonal, the
+ *              superdiagonal (above the main diagonal), and the
+ *              subdiagonal (below the main diagonal).
+ *
+ * Members:
+ * - size: The dimension 'n' of the n x n matrix.
+ * - arr:  A one-dimensional array storing the non-zero elements.
+ *
+ * Example of a 4x4 tridiagonal matrix:
+ *
+ *     | b1  c1   0   0 |
+ *     | a1  b2  c2   0 |
+ *     |  0  a2  b3  c3 |
+ *     |  0   0  a3  b4 |
+ *
+ * Storage order in 'arr':
+ * arr = [c1, c2, c3, b1, b2, b3, b4, a1, a2, a3]
+ */
 typedef struct TridiagonalMatrix TridiagonalMatrix;
 
 TridiagonalMatrix *tridiagonal_matrix_new(size_t size);
