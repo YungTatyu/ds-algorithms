048848d1e6c422943a81a4749edeb799d4fdd945
tachu <roatyungtatyu@gmail.com>
2025-03-10 01:32:37 +0900

    add: description

--- matrix/toeplitz_matrix/toeplitz_matrix.h
+++ matrix/toeplitz_matrix/toeplitz_matrix.h
@@ -7,6 +7,31 @@ struct ToeplitxMatriz {
   size_t size;
 };
 
+/**
+ * Structure: ToeplitzMatrix
+ * Description: Represents a Toeplitz matrix, which is a matrix where each
+ *              descending diagonal from left to right is constant. This
+ *              means that all elements along a diagonal have the same value.
+ *
+ * Members:
+ * - size: The dimension 'n' of the n x n matrix.
+ * - arr:  A one-dimensional array storing the unique elements of the
+ *         Toeplitz matrix. For an n x n Toeplitz matrix, 'arr' contains
+ *         2n - 1 elements: the first 'n' elements correspond to the first
+ *         row, and the remaining 'n - 1' elements correspond to the first
+ *         column (excluding the first element, as it's already included in
+ *         the first row).
+ *
+ * Example of a 4x4 Toeplitz matrix:
+ *
+ *     | a  b  c  d |
+ *     | e  a  b  c |
+ *     | f  e  a  b |
+ *     | g  f  e  a |
+ *
+ * Storage order in 'arr':
+ * arr = [a, b, c, d, e, f, g]
+ */
 typedef struct ToeplitxMatriz ToeplitxMatriz;
 
 ToeplitxMatriz *toeplitz_matrix_new(size_t size);
