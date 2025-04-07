357bb89066c32d901d78583ee9eb06e40fb6a07b
tachu <roatyungtatyu@gmail.com>
2025-04-07 22:58:01 +0900

    add: circular_queue

---
+++ queue/circular_queue/queue.c
@@ -0,0 +1,62 @@
+#include "queue.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+Queue *queue_new(size_t size) {
+  Queue *q = calloc(1, sizeof(Queue));
+  if (q == NULL) {
+    return NULL;
+  }
+  q->q = calloc(size, sizeof(int));
+  if (q->q == NULL) {
+    free(q);
+    return NULL;
+  }
+  q->size = size;
+  return q;
+}
+
+void queue_delete(Queue *q) {
+  free(q->q);
+  free(q);
+}
+
+size_t queue_index(const Queue *q, size_t index) { return index % q->size; }
+
+size_t queue_size(const Queue *q) { return q->size; }
+
+int queue_empty(const Queue *q) { return q->front == q->rear; }
+
+int queue_full(const Queue *q) {
+  return q->front == queue_index(q, q->rear + 1);
+}
+
+void queue_enqueue(Queue *q, int v) {
+  if (queue_full(q)) {
+    return;
+  }
+  q->q[q->rear] = v;
+  q->rear = queue_index(q, q->rear + 1);
+}
+
+int queue_dequeue(Queue *q) {
+  if (queue_empty(q)) {
+    return -1;
+  }
+  int re = q->q[q->front];
+  q->front = queue_index(q, q->front + 1);
+  return re;
+}
+
+void queue_display(const Queue *q) {
+  if (queue_empty(q)) {
+    return;
+  }
+  size_t i = q->front;
+  while (i != q->rear) {
+    printf("%d ", q->q[i]);
+    i = queue_index(q, i + 1);
+  }
+  printf("\n");
+}
---
+++ queue/circular_queue/queue.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <stddef.h>
+
+struct Queue {
+  size_t size;
+  size_t front;
+  size_t rear;
+  int *q;
+};
+
+typedef struct Queue Queue;
+
+Queue *queue_new(size_t size);
+
+void queue_delete(Queue *q);
+
+size_t queue_index(const Queue *q, size_t index);
+
+void queue_enqueue(Queue *q, int v);
+
+int queue_dequeue(Queue *q);
+
+size_t queue_size(const Queue *q);
+
+int queue_empty(const Queue *q);
+
+int queue_full(const Queue *q);
+
+void queue_display(const Queue *q);
