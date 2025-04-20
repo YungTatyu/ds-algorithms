c08875d72acb07d32cde5645d7906432eb2739d0
tachu <roatyungtatyu@gmail.com>
2025-04-20 15:55:31 +0900

    refactor

--- queue/stack/main.c
+++ queue/stack/main.c
@@ -6,7 +6,13 @@ int main(int argc, char *argv[]) {
   for (int i = 0; i < 10; ++i) {
     queue_enqueue(q, i);
   }
-  for (int i = 0; i < 10; ++i) {
+  for (int i = 0; i < 5; ++i) {
+    printf("%d ", queue_dequeue(q));
+  }
+  for (int i = 10; i < 20; ++i) {
+    queue_enqueue(q, i);
+  }
+  for (int i = 0; i < 20; ++i) {
     printf("%d ", queue_dequeue(q));
   }
   return 0;
