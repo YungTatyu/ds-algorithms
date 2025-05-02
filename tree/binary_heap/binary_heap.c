9aca71a13aa69de4b9dd72a0a4118db1cb65c7a8
tachu <roatyungtatyu@gmail.com>
2025-05-02 20:04:53 +0900

    refactor

--- tree/binary_heap/binary_heap.c
+++ tree/binary_heap/binary_heap.c
@@ -1,5 +1,17 @@
 #include "binary_heap.h"
 
+/**
+ * @brief Inserts a new element into the heap.
+ *
+ * This function treats the given array as a max-heap and inserts
+ * a new element while maintaining the heap property.
+ *
+ * @param arr An integer array representing the heap.
+ * @param index The index of the element to be inserted.
+ *
+ * @note The array `arr` must have sufficient allocated size,
+ *       and `index` must be within the valid range of the array.
+ */
 void heap_insert(int arr[], size_t index) {
   int new_value = arr[index];
   while (index > 0) {
